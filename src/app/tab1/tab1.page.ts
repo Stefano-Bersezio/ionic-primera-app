ecd3a5a4dc2a05badf3e9d518189c56ea8e58ce6
Stefano Bersezio <s.bersezio@gmail.com>
2022-04-13 14:18:51 -0400

    Fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page{
 
   constructor() {}
 
+ ngOnInit(){
+
+  let numero = 10;
+  // debugger;
+
+  numero = 20;
+
+  numero = numero + 10;
+
+  numero = 0;
+
+   console.log(numero);
+ }
+
 }
+
+// Con el ngOnInit y el console log puedo visualizar por consola
+
+// debugger solo al ponerlo al recargar la pagina se detendra en el punto donde colocamos ese debugger
